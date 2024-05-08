a6c071ab25e950dcd6a6227aa19bcfcc7758e633
allenhouu <126ahou@frhsd.com>
2024-05-08 12:53:06 -0400

    update

--- calc.js
+++ calc.js
@@ -6,11 +6,31 @@ function displayNum(value)
 {
     document.getElementById("display").value += value;
 }
+function addition()
+{
+    document.getElementById("display").value+= "+";
+}
+function subtraction()
+{
+    document.getElementById("display").value+= "-";
+}
+function division()
+{
+    document.getElementById("display").value+= "/";
+}
+function multiplication()
+{
+    document.getElementById("display").value+= "*";
+}
+function sqrt()
+{
+    document.getElementById("display").value+= "√";
+}
 function backspace()
 {
 
 }
 function enter()
 {
-
+    eval(value);
 }
