74feae79eab18acbe1e78a579adefd138f6d8e17
allenhouu <126ahou@frhsd.com>
2024-05-08 12:44:25 -0400

    update

---
+++ calc.js
@@ -0,0 +1,16 @@
+function clear()
+{
+    document.getElementById("display").value = "";
+}
+function displayNum(value)
+{
+    document.getElementById("display").value += value;
+}
+function backspace()
+{
+
+}
+function enter()
+{
+
+}
