1bc91e28787f7777eda00e43b9168a63def9e4bd
allenhouu <126ahou@frhsd.com>
2024-05-09 14:05:05 -0400

    update

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-function clear()
+function clearCalc()
 {
     document.getElementById("display").value = "";
 }
@@ -24,13 +24,42 @@ function multiplication()
 }
 function sqrt()
 {
-    document.getElementById("display").value+= "√";
+    document.getElementById("display").value += "Math.sqrt(";
+}
+function exponent()
+{
+    document.getElementById("display").value += "**";
 }
 function backspace()
 {
-
+    let nums = document.getElementById("display").value;
+    let sliced = nums.slice(0,-1);
+    document.getElementById("display").value = sliced;
+}
+function decimal()
+{
+    document.getElementById("display").value += ".";
+}
+function openP()
+{
+    document.getElementById("display").value += "(";
+}
+function closeP()
+{
+    document.getElementById("display").value += ")";
 }
 function enter()
 {
-    eval(value);
+    try
+    {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    }
+    catch (error)
+    {
+        document.getElementById("display").value = "n/a";
+    }
+}
+function history()
+{
+
 }
